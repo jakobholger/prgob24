1a14018f934f3e1d15df1cb5c8947412e1a3f674
jakobholger <jakob.akervall@gmail.com>
2024-09-05 14:59:57 +0200

    Fixed repetition

--- lab4/chomptest.py
+++ lab4/chomptest.py
@@ -1,13 +1,3 @@
-#CLI instruktioner för spelarna
-print("\nVälkommen till spelet Chomp.\n")
-print("Målet med spelet är att inte äta chokladrutan med värdet P. Poison chokladbiten.")
-print("När det är din tur väljer du en ruta genom att skriva in värdet på rutan i terminalen.")
-print("Alla rutor till höger och under om denna ruta kommer då att ätas.")
-print("Därefter är det Spelare 2 tur att välja en ruta och äta.")
-print("Detta fortsätter till en av spelarna tvingas äta Poison chokladbiten och förlorar.\n")
-print("Lycka till!\n")
-input("Tryck ENTER för att börja.\n")
-
 #Funktioner
 def create_chocolate_bar(row, col):
     if row<=0 or col<=0:
@@ -20,7 +10,7 @@ def create_chocolate_bar(row, col):
     for i in range(0,row):
         col_integer=1
         for j in range(0,col):
-            matrix[i][j] = str(10*row_integer+col_integer)
+            matrix[i][j] = str(10*row_integer+col_integer) #Strängar av 10*raden + kolumheltalet
             col_integer+=1
         row_integer+=1
     matrix[0][0] = "P"
@@ -29,7 +19,7 @@ def create_chocolate_bar(row, col):
 def print_chocolate_bar(matrix):
     for row in matrix:
         for col in row:
-            print(f"{col:>3}",end="")
+            print(f"{col:>3}",end="") #Se till att outputen är formatterad med strängar som är 3 karaktärer långa
         print()
     return None
 
@@ -44,12 +34,12 @@ def chomp(matrix, row_index, col_index):
     #Om en rad är helt tom så tas den bort från arrayn
     matrix = [row for row in matrix if row]
 
-    if not matrix:
+    if not matrix: #Om den är tom retunera en tom lista
         return []
     return matrix
 
 def check_winner(matrix):
-    if len(matrix) == 1 and len(matrix[0])==1:
+    if len(matrix) == 1 and len(matrix[0])==1: #Om antalet antalet rader och kolumer är 1 så har någon vunnit.
         return True
     return False
 
@@ -59,52 +49,57 @@ def ask_cell_number(matrix):
             # Be användaren mata in ett nummer
             user_input = input("Ange ett nummer som finns i matrisen: ")
             if user_input=="P":
-                # Om numret inte hittas i matrisen, kasta ett undantag
+                # Om indatan är P så är valueerrorn att man inte får förstöra för sig själv
                 raise ValueError("Självförstörelse ej tillåtet, försök igen!")
-            # Hitta rad och kolumn där numret finns
-            for i, row in enumerate(matrix):
-                if user_input in row:
-                    # Returnera raden och kolumnen som tuple om numret hittas
-                    return (i, row.index(user_input))
             
+            # Hitta rad och kolumn där numret finns
+            for row in range(0,len(matrix)):
+                for col in range(0,len(matrix[row])):
+                    if user_input in matrix[row][col]:
+                        return (row,col)
+
             # Om numret inte hittas i matrisen, kasta ett undantag
             raise ValueError("Fel val, ruta " + user_input + " finns inte i spelplanen, försök igen!")
         
         except ValueError as e:
             print(e)  # Skriv ut felmeddelandet och fråga igen
 
+#Funktion som frågar efter antalet rader och kolumer och returnerar de i int form om dem är giltiga
+def prompt_matrix_size():
+    #Vi sätter upp en lista av de giltiga inmatnings integerna i strängform för matrisen
+    valid_numbers = ["2","3","4","5","6","7","8","9"]
 
-#Vi sätter upp en lista av de giltiga inmatnings integerna i strängform
-valid_numbers = ["1","2","3","4","5","6","7","8","9"]
+    matrix_width = ""
+    matrix_height = ""
 
-def validate_matrix_input(input):
-    if input not in valid_numbers:
-        return False
-    return True
+    matrix_input_valid = False
 
-matrix_width = ""
-matrix_height = ""
+    while not matrix_input_valid:
+        matrix_width = input("Hur många rader ska chokladbaren bestå av (2-9): ")
+        matrix_height = input("Hur många kolumer ska chokladbaren bestå av (2-9): ")
+        if matrix_width in valid_numbers and matrix_height in valid_numbers:
+            matrix_input_valid = True
+            return int(matrix_width),int(matrix_height)
+        else:
+            print("Inmata ett giltigt nummer. Testa igen.")
 
-matrix_input_valid = False
 
-while not matrix_input_valid:
-    matrix_width = input("Hur många rader ska chokladbaren bestå av: ")
-    matrix_height = input("Hur många kolumer ska chokladbaren bestå av: ")
-    if validate_matrix_input(matrix_width) and validate_matrix_input(matrix_height):
-        matrix_input_valid = True
-        matrix_width = int(matrix_width)
-        matrix_height = int(matrix_height)
-    else:
-        print("Inmata ett giltigt nummer. Testa igen.")
+#CLI instruktioner för spelarna
+print("\nVälkommen till spelet Chomp.\n")
+print("Målet med spelet är att inte äta chokladrutan med värdet P. Poison chokladbiten.")
+print("När det är din tur väljer du en ruta genom att skriva in värdet på rutan i terminalen.")
+print("Alla rutor till höger och under om denna ruta kommer då att ätas.")
+print("Därefter är det Spelare 2 tur att välja en ruta och äta.")
+print("Detta fortsätter till en av spelarna tvingas äta Poison chokladbiten och förlorar.\n")
+print("Lycka till!\n")
+input("Tryck ENTER för att börja.\n")
+
+matrix_width, matrix_height = prompt_matrix_size()
 
 matrix = create_chocolate_bar(matrix_width, matrix_height)
 
 #Gameloop
-play_again_bool = False
-def game(matrix):
-    global play_again_bool
-    play_again_bool = False
-    
+def game(matrix): 
     #Loop
     while True:
         #Om tidigare drag resulterade i att spelare 1 förlorade, vann spelare 2
@@ -126,14 +121,18 @@ def game(matrix):
         print("\nSpelare 2 tur")
         row,col = ask_cell_number(matrix)
         matrix = chomp(matrix,row,col) 
+game(matrix) #Inkalla funktionen
 
+#Spela igen loop
+play_again_bool = True
+while play_again_bool:
     restart = input("För att spela igen inmata 1. För att stänga programmet tryck ENTER.")    
     if restart == "1":
-        play_again_bool = True  
-game(matrix) 
 
-#Återställ matrisen till sitt ursprunliga tillstånd
-while play_again_bool:
-    matrix = create_chocolate_bar(matrix_width,matrix_height)
-    game(matrix)
+        matrix_width,matrix_height = prompt_matrix_size()
+        matrix = create_chocolate_bar(matrix_width,matrix_height)
+        game(matrix)
+        play_again_bool = True  
+    else:
+        break
 
